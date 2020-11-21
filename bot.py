7ec0525f3638ea28115efc3bbe0c955bb7f4bc07
Raj Jaiswal <49625158+ryugaraj@users.noreply.github.com>
2020-11-21 16:51:15 +0530

    different users added

--- bot.py
+++ bot.py
@@ -1,34 +1,56 @@
 from instapy import InstaPy
 from selenium import webdriver
 import os
-#Login 
-driver = webdriver.Firefox(executable_path=os.environ.get("FIREFOX_BIN"))
-session = InstaPy(username="scarlettjohansonoffcial", password="kj816g197a",
-                            geckodriver_path = os.environ.get("GECKODRIVER_PATH"), browser_executable_path=os.environ.get("FIREFOX_BIN"),
-                            headless_browser = True)
+import time
+def runner1():
+  session = InstaPy(username=os.environ.get("runner1.u"), password=os.environ.get("runner1.p"),
+                              geckodriver_path = os.environ.get("GECKODRIVER_PATH"), browser_executable_path=os.environ.get("FIREFOX_BIN"),
+                              headless_browser = True)
 
-session.login()
+  session.login()
+  session.set_quota_supervisor(enabled=True,
+                               peak_likes_hourly=70,
+                               peak_comments_hourly=21,
+                               peak_follows_hourly=48,
+                               peak_follows_daily=100,
+                               peak_unfollows_hourly=35,
+                               peak_unfollows_daily=402)
 
-#Setting
-session.set_quota_supervisor(enabled=True,
-                             peak_likes_hourly=57,
-                             peak_comments_hourly=21,
-                             peak_follows_hourly=48,
-                             peak_follows_daily=None,
-                             peak_unfollows_hourly=35,
-                             peak_unfollows_daily=402)
+  session.like_by_tags(['scarlettjohansson'], amount=30)
+  session.set_do_comment(True, percentage=100)
+  session.set_comments([u'My Girl is fire :fire:',
+                        u'So Awesome :flushed:',
+                        u'Amazing :heart_eyes:'])
 
+  session.follow_user_followers(['scarlettjohanssonworld'], amount=50,
+                                randomize=False, sleep_delay=10)
+  '''session.unfollow_users(amount=30, allFollowing=True, style="LIFO",
+                         unfollow_after=24*60*60, sleep_delay=5)'''
+  session.end()
 
+def runner2():
+  session = InstaPy(username=os.environ.get("runner2.u"), password=os.environ.get("runner2.p"),
+                              geckodriver_path = os.environ.get("GECKODRIVER_PATH"), browser_executable_path=os.environ.get("FIREFOX_BIN"),
+                              headless_browser = True)
 
-session.set_do_comment(True, percentage=100)
-session.set_comments([u'My Girl is fire :fire:',
-                      u'So Awesome :flushed:',
-                      u'Amazing :heart_eyes:'])
+  session.login()
+  session.set_quota_supervisor(enabled=True,
+                               peak_likes_hourly=70,
+                               peak_comments_hourly=21,
+                               peak_follows_hourly=48,
+                               peak_follows_daily=100,
+                               peak_unfollows_hourly=35,
+                               peak_unfollows_daily=402)
 
-#Action
-session.like_by_tags(['scarlettjohansson'], skip_top_posts=True, amount=20)
-session.follow_user_followers(['scarlettjohanssonworld'], amount=40,
-                              randomize=False, sleep_delay=30)
-#session.unfollow_users(amount=35, allFollowing=True, style="LIFO",
-#                       unfollow_after=24*60*60, sleep_delay=5)
-session.end()
+  session.like_by_tags(['coder'], amount=30)
+  session.set_do_comment(True, percentage=100)
+  session.set_comments([u'Nice :fire:',
+                        u'So Awesome :flushed:',
+                        u'Amazing :heart_eyes:'])
+
+  session.follow_user_followers(['codecademy'], amount=50,
+                                randomize=False, sleep_delay=10)
+while 1:
+  runner1()
+  runner2()
+  time.sleep(3600)
