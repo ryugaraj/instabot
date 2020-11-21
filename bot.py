2bc1f4a72e774810ee3c5e71257cff84352fd55d
Sumit Jaiswal <54527500+ransomsumit@users.noreply.github.com>
2020-11-21 14:34:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,25 +9,20 @@ def runner():
                               headless_browser = True)
 
   session.login()
-
-  #Setting
   session.set_quota_supervisor(enabled=True,
-                               peak_likes_hourly=57,
+                               peak_likes_hourly=70,
                                peak_comments_hourly=21,
                                peak_follows_hourly=48,
-                               peak_follows_daily=40,
+                               peak_follows_daily=100,
                                peak_unfollows_hourly=35,
                                peak_unfollows_daily=402)
 
-
-
+  session.like_by_tags(['scarlettjohansson'], skip_top_posts=True, amount=20)
   session.set_do_comment(True, percentage=100)
   session.set_comments([u'My Girl is fire :fire:',
                         u'So Awesome :flushed:',
                         u'Amazing :heart_eyes:'])
 
-  #Action
-  session.like_by_tags(['scarlettjohansson'], skip_top_posts=True, amount=20)
   '''session.follow_user_followers(['scarlettjohanssonworld'], amount=40,
                                 randomize=False, sleep_delay=30)
   #session.unfollow_users(amount=35, allFollowing=True, style="LIFO",
