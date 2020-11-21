9190f367a0ddf4376dd3dca14e8f8a821516cb64
Sumit Jaiswal <54527500+ransomsumit@users.noreply.github.com>
2020-11-21 14:21:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,38 @@
 from instapy import InstaPy
 from selenium import webdriver
 import os
+import time
 #Login 
-#driver = webdriver.Firefox(executable_path=os.environ.get("FIREFOX_BIN"))
-session = InstaPy(username="scarlettjohansonoffcial", password="kj816g197a",
-                            geckodriver_path = os.environ.get("GECKODRIVER_PATH"), browser_executable_path=os.environ.get("FIREFOX_BIN"),
-                            headless_browser = True)
+def runner():
+  session = InstaPy(username="scarlettjohansonoffcial", password="kj816g197a",
+                              geckodriver_path = os.environ.get("GECKODRIVER_PATH"), browser_executable_path=os.environ.get("FIREFOX_BIN"),
+                              headless_browser = True)
 
-session.login()
+  session.login()
 
-#Setting
-session.set_quota_supervisor(enabled=True,
-                             peak_likes_hourly=57,
-                             peak_comments_hourly=21,
-                             peak_follows_hourly=48,
-                             peak_follows_daily=None,
-                             peak_unfollows_hourly=35,
-                             peak_unfollows_daily=402)
+  #Setting
+  session.set_quota_supervisor(enabled=True,
+                               peak_likes_hourly=57,
+                               peak_comments_hourly=21,
+                               peak_follows_hourly=48,
+                               peak_follows_daily=40,
+                               peak_unfollows_hourly=35,
+                               peak_unfollows_daily=402)
 
 
 
-session.set_do_comment(True, percentage=100)
-session.set_comments([u'My Girl is fire :fire:',
-                      u'So Awesome :flushed:',
-                      u'Amazing :heart_eyes:'])
+  session.set_do_comment(True, percentage=100)
+  session.set_comments([u'My Girl is fire :fire:',
+                        u'So Awesome :flushed:',
+                        u'Amazing :heart_eyes:'])
 
-#Action
-session.like_by_tags(['scarlettjohansson'], skip_top_posts=True, amount=20)
-session.follow_user_followers(['scarlettjohanssonworld'], amount=40,
-                              randomize=False, sleep_delay=30)
-#session.unfollow_users(amount=35, allFollowing=True, style="LIFO",
-#                       unfollow_after=24*60*60, sleep_delay=5)
-session.end()
+  #Action
+  session.like_by_tags(['scarlettjohansson'], skip_top_posts=True, amount=20)
+  '''session.follow_user_followers(['scarlettjohanssonworld'], amount=40,
+                                randomize=False, sleep_delay=30)
+  #session.unfollow_users(amount=35, allFollowing=True, style="LIFO",
+  #                       unfollow_after=24*60*60, sleep_delay=5)'''
+  session.end()
+while 1:
+  runner()
+  time.sleep(60)
