33e1eb905b40c0a39455ecad4f7c490b70c8a2e9
Raj Jaiswal <49625158+ryugaraj@users.noreply.github.com>
2020-12-12 21:17:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,46 +1,47 @@
-import os
 from time import sleep
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from random import randint
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-
-options = Options()
-options.headless = True
-binary = FirefoxBinary(os.environ.get("FIREFOX_BIN"))
-browser = webdriver.Firefox(options=options, firefox_binary=binary, executable_path=os.environ.get("GECKODRIVER_PATH"))
-browser.implicitly_wait(5)
-browser.get('https://www.instagram.com/')
-
-sleep(2)
-
 
 #Login
 
-def login():
-    
-    username_input = browser.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input')
-    username_input.send_keys(os.environ.get("runner1.u"))
-    password_input = browser.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input')
-    password_input.send_keys(os.environ.get("runner1.p"))
-    login_button = browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]")
-    login_button.click()
-    browser.implicitly_wait(5)
-    not_now = browser.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div/div/button')
-    not_now.click()
-    browser.implicitly_wait(5)
-    not_now2 = browser.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')
-    not_now2.click()
+browser = webdriver.Firefox()
+browser.implicitly_wait(5)
+browser.get('https://www.instagram.com/')
 
-login()
+sleep(60)
+
+username_input = browser.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input')
+username_input.send_keys("angelverma007")
+password_input = browser.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input')
+password_input.send_keys("hgku4577")
+
+login_button = browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]")
+login_button.click()
+browser.implicitly_wait(5)
+not_now = browser.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div/div/button')
+not_now.click()
+browser.implicitly_wait(5)
+not_now2 = browser.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')
+not_now2.click()
+
 
 #Comments and Id's 
 
-comments=['Wanna have some fun with me tonight 🍑💦',"Don't you dare see my story 🍑💦 ","I can be your bad girl tonight 🍑💦", "See my Big surprise dear 🍑🍑 it's waiting for you💦💦",
-          "I want that big long and hard 🍌🍌💦💦","I wanna see a big 🍆🍆 and play with it💦💦 ","Everyone who reads this be happy ✨✨✨✨"]
+comments=[ 'Hello koi apna bada sa lund dikhaega 🍆🍆💦💦', 'Follow Me whatsapp pe chat krenge ✉️✉️ ',
+ 'Dm me aa jaiye 🙈 ek dusre ka dekhenge 🍆🍑','Aa jao bahut garam hu abhi 🍆💦', 'Kisi ke paas bada aur lamba lund hai to dm kre🍆💦',
+'Sirf bade lund wale 🍆💦 Boys Follow kre 💋💋','main kaisi lgti hu 🍑 jara dekh ke batao 👙👙', 
+'sab mujhe follow kro ❤️ jo chahe dungi🍑🍆💦 ', 'follow krke dm me aa jao 🙈 tumhare hosh uda dungi', 
+'mere bf ka khada nhi hota hai koi hai jo meri pyaas bujha de 🍆🍆💦💦', 'Bahut horny hu yaar 🍆💦 jldi aao na dm me 💋💋',
+'mera badan tadap rha hai tumhara 🍆💦 dekhne ko', 'koi to meri pyaas bujha do 🍆💦', 'Dm me aao follo krke 🙈 🍆💦',
+ 'koi mard hai bde lund wala jo meri jawani ki aag ko bujha sake 🍆💦🍆💦'] 
 
-insta_id=['https://www.instagram.com/9gag/'] 
+insta_id=['https://www.instagram.com/neha_rajput___143/','https://www.instagram.com/dimpalankita/',
+'https://www.instagram.com/muskan_rajput517/','https://www.instagram.com/nisha_cute199/','https://www.instagram.com/renu__kumari_/',
+'https://www.instagram.com/bindas_pooja/','https://www.instagram.com/muskan__varma/','https://www.instagram.com/pooja_sharma921/',
+'https://www.instagram.com/nisha_sharma_57/','https://www.instagram.com/anita_kumari9/','https://www.instagram.com/kajal.sharmafc/',
+'https://www.instagram.com/alia_7569/','https://www.instagram.com/kaurmeet.1313/','https://www.instagram.com/vandana__devi/']
+
 
 def run():
     num_comment=0
@@ -62,25 +63,22 @@ def run():
                     so = soup2.find('div',class_="k_Q0X NnvRN")
                     timer = so.find('time',class_="_1o9PC Nzb55").text
                 
-                    if ("second" in timer):
+                    if ('second' in timer )  or (("min" in timer) and ((timer[0]=='1')or (timer[0]=='2'))):
                         num_comment+=1
                         like=browser.find_element_by_xpath("/html/body/div[1]/section/main/div/div/article/div[3]/section[1]/span[1]/button")
                         like.click()
 
                         browser.find_element_by_class_name('X7cDz').click()
-                        browser.find_element_by_xpath("/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[3]/div/form/textarea").send_keys(comments[randint(0,6)])
+                        browser.find_element_by_xpath("/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[3]/div/form/textarea").send_keys(comments[randint(0,1)])
                         
                         post_btn=browser.find_element_by_xpath("/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[3]/div/form/button")
                         post_btn.click()
-                        
-                        sleep(60)
                 
                     if num_comment >=15:
                         num_comment=0
                         sleep(600)
-            
-    except Exception as e:
-        print(e.__class__)
+                sleep(0.5)
+    except:
         run()
 run()
 
